e86ea4c81e4da743448e10ec0a5b17784c36de07
BJuice <beetle_juice84@mail.ru>
2022-11-16 02:14:08 +0300

    fix/sameite: 'none' for controller users.js

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -118,7 +118,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 7 * 24,
         httpOnly: true,
-        sameSite: true,
+        sameSite: 'none',
         secure: true,
       })
         .send({ token });
